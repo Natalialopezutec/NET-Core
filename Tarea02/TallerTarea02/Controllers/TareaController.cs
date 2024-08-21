464fa391ca81c52a92e6bde494a4383232e6b4d6
nlopez@antel.com.uy <natalia.lopez@estudiantes.utec.edu.uy>
2024-08-21 02:01:19 -0300

    Respaldo
    Tarea 02 terminada

---
+++ Tarea02/TallerTarea02/Controllers/TareaController.cs
@@ -0,0 +1,58 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Xml.Linq;
+using TallerTarea02.Models;
+
+namespace TallerTarea02.Controllers
+{
+    [ApiController]
+    [Route("api/tareas")]
+
+    public class TareaController : Controller
+    {
+        private readonly ILogger<TareaController> _logger;
+
+        private IList<Tarea> _tareaList;
+
+        public TareaController(ILogger<TareaController> logger)
+        {
+            _logger = logger;
+            this._tareaList = new List<Tarea>();
+            this._tareaList.Add(new Tarea(1, "EDA", "programacion", 4, "Gualberto"));
+            this._tareaList.Add(new Tarea(2, "Taller Java", "programacion", 6, "Gabriel"));
+            this._tareaList.Add(new Tarea(3, "Programacion Avanzada", "programacion", 12, "Juan"));
+            this._tareaList.Add(new Tarea(4, "Programacion Aplicaciones", "programacion", 4, "Andres"));
+        }
+
+        [HttpGet]
+        public ActionResult<IList<Tarea>> GetAll()
+        {
+            _logger.LogInformation("Lista de todas las tareas");
+            return Ok(this._tareaList.ToList());
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Tarea> GetById(int id)
+        {
+            _logger.LogInformation($"Tarea por Id {id}");
+            return Ok(this._tareaList.FirstOrDefault(t => t.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult NuevaTarea([FromBody]Tarea tarea)
+        {
+            _logger.LogInformation("Tarea nueva");
+            this._tareaList.Add(tarea);
+            return Ok();
+        }
+
+        [HttpDelete ("{id}")]
+        public ActionResult DeleteById(int id)
+        {
+            _logger.LogInformation("Tarea borrado por id");
+            this._tareaList.RemoveAt(id);
+            return Ok();
+        }
+    }
+}
---
+++ Tarea02/TallerTarea02/Models/Tarea.cs
@@ -0,0 +1,39 @@
+﻿namespace TallerTarea02.Models
+{
+    public class Tarea // id, nombre, desc, duración horas, responsabl
+    {
+        private long _id; //campo privado
+        public long Id
+        {
+            set { _id = value; }
+            get { return _id; } //propiedad suele estar asociadas
+                                //a un campo privado de la clase
+        }
+        public string? Nombre { get; set; } //atributo opcional tipo string
+        public string? Desc { get; set; }
+        public int? Duracion { get; set; } //atributo opcional tipo entero
+        public string? Responsable { get; set; }
+        public DateTime? Fecha { get; set; }// Nuevo atributo de tipo fecha
+
+        public Tarea(int id, string nombre, string desc, int duracion, string responsable)
+        {
+            this._id = id;
+            this.Nombre = nombre;
+            this.Desc = desc;
+            this.Duracion = duracion;
+            this.Responsable = responsable;
+        }
+        public Tarea(int id, string nombre, string desc, int duracion, string responsable, DateTime? fecha) 
+        { 
+            this._id = id;
+            this.Nombre = nombre;
+            this.Desc = desc;   
+            this.Duracion = duracion;
+            this.Responsable = responsable;
+            this.Fecha = fecha;
+        }
+
+        public Tarea() { }
+
+    }
+}
